ffd6a06f9761f0e90fa52130206a9784448482dd
gkgktmd <53550224+gkgktmd@users.noreply.github.com>
2021-04-05 21:08:44 +0900

    Add files via upload

---
+++ 733회부터_ver3_210329.py
@@ -0,0 +1,107 @@
+#733회부터
+import random
+import time
+start=time.time()
+
+hr22=[12,38,18,43,10,42,39,34,17,16,21,30,13,36,3,33,31,4,24,19,45,26] #상위 22개
+lr23=[41,28,9,6,32,27,15,11,14,22,2,5,35,29,7,44,23,40,37,25,1,20,8] #하위 23개
+
+hr38=[12,38,18,43,10,42,39,34,17,16,21,30,13,36,3,33,31,4,24,19,45,26,41,28,9,6,32,27,15,11,14,22,2,5,35,29,7,44] #상위 38개
+lr7=[23,40,37,25,1,20,8] #하위 7개
+
+def get_count(x):
+    new_list={}
+    for i in x:
+        try: new_list[i]+=1
+        except: new_list[i]=1
+    return new_list
+
+count=0
+while True:
+    # 1번 모델 중복 제거
+    while True:
+        a1 = sorted(random.sample(hr38, 4) + random.sample(lr7, 2))
+        a11 = get_count(a1)
+        a = [0] * 10
+        for key, value in a11.items():
+            if value > 1:
+                a[value] += 1
+        if a[2] <= 0 and a[3] <= 0 and a[4] == 0 and a[5] == 0:
+            break
+
+    # 2번 모델 중복 제거
+    while True:
+        a2 = sorted(random.sample(hr38, 3) + random.sample(lr7, 3))
+        a22 = get_count(a2)
+        a = [0] * 10
+        for key, value in a22.items():
+            if value > 1:
+                a[value] += 1
+        if a[2] <= 0 and a[3] <= 0 and a[4] == 0 and a[5] == 0:
+            break
+
+    # 3번 모델 중복 제거
+    while True:
+        a3 = sorted(random.sample(hr22, 4) + random.sample(lr23, 2))
+        a33 = get_count(a3)
+        a = [0] * 10
+        for key, value in a33.items():
+            if value > 1:
+                a[value] += 1
+        if a[2] <= 0 and a[3] <= 0 and a[4] == 0 and a[5] == 0:
+            break
+
+    # 4번 모델 중복 제거
+    while True:
+        a4 = sorted(random.sample(hr22, 2) + random.sample(lr23, 4))
+        a44 = get_count(a4)
+        a = [0] * 10
+        for key, value in a44.items():
+            if value > 1:
+                a[value] += 1
+        if a[2] <= 0 and a[3] <= 0 and a[4] == 0 and a[5] == 0:
+            break
+
+    # 5번 모델 중복 제거
+    while True:
+        a5 = sorted(random.sample(hr22, 3) + random.sample(lr23, 3))
+        a55 = get_count(a5)
+        a = [0] * 10
+        for key, value in a55.items():
+            if value > 1:
+                a[value] += 1
+        if a[2] <= 0 and a[3] <= 0 and a[4] == 0 and a[5] == 0:
+            break
+
+    # 쓰까 모델 추출
+    num = a1 + a2 + a3 + a4 + a5
+    d1 = get_count(num)
+
+    a = [0] * 10
+    for key, value in d1.items():
+        if value > 1:
+            a[value] += 1
+    print(a)
+    count+=1
+    if a[2] <= 0 and a[3] <= 0 and a[4] == 0 and a[5]==0:
+        break
+
+print('-'*30)
+print(f'1-1: {len(hr22)}개 1-2: {len(lr23)}개')
+
+a22=sorted(hr22+lr23)
+
+if a22==list(range(1,46)):
+    print('대상 True')
+
+print('-'*30)
+print(f'돌린 횟수: {count}')
+print(f'걸린 시간: {round(time.time()-start,3)}초')
+print('-'*30)
+b = [0] * 5
+for i in range(5):
+    if a[i] != 0:
+        b[i] += a[i]
+        print(f'{i}번 나온 숫자가 총 {a[i]}개')
+print('-'*30)
+print(f'#1 {a1}',f'#2 {a2}',f'#3 {a3}',f'#4 {a4}',f'#5 {a5}',sep='\n')
---
+++ 가중치_210331.py
@@ -0,0 +1,81 @@
+import random
+import time
+from collections import Counter
+start=time.time()
+
+holy_num=list(range(1,46))
+
+#기존 숫자별 가중치
+# weight=[17883,25967,25123,26088,26365,30368,20443,32463,30563
+# ,49863,35766,43850,43006,43971,44248,48251,38326,50346,48446,57947,43850
+# ,51934,51090,52055,52332,56335,46410,58430,56530,57103,43006,51090,50246
+# ,51211,51488,55491,45566,57586,55686,58068,43971,52055,51211,52176,52453]
+
+#최대치에서 각 값 빼준 뒤 그 차이를 줄이는 방식(나누기 5000)
+weight=[58422,58424,58423,58424,58424,58424,58422,58425,58424,58428,58425,58427
+,58427,58427,58427,58428,58426,58428,58428,58430,58427,58429,58429,58429,58429
+,58430,58428,58430,58430,58430,58427,58429,58428,58429,58429,58429,58427,58430
+,58429,58430,58427,58429,58429,58429,58429]
+
+def get_count(x):
+    gc_list={}
+    for i in x:
+        try: gc_list[i]+=1
+        except: gc_list[i]=1
+    return gc_list
+
+count=0
+while True:
+    c=0
+    winner=[]
+
+    while c<=5:
+        new_list={}
+        num=0
+        while num<1000:
+            a1=sorted(random.choices(holy_num,weight,k=6))
+            for i in a1:
+                try: new_list[i]+=1
+                except: new_list[i]=1
+            num+=1
+            #print(a1)
+
+        result=dict(sorted(new_list.items(), key=lambda x : x[1], reverse=True))
+
+        aa = []
+        for key in result:
+            aa.append(key)
+        winner.append(aa[0:6])
+        c+=1
+
+    a1 = sorted(winner[0])
+    a2 = sorted(winner[1])
+    a3 = sorted(winner[2])
+    a4 = sorted(winner[3])
+    a5 = sorted(winner[4])
+
+    num = a1 + a2 + a3 + a4 + a5
+    d1 = get_count(num)
+
+    a = [0] * 10
+    for key, value in d1.items():
+        if value > 1:
+            a[value] += 1
+    print(a)
+    count+=1
+    if a[2] <= 1 and a[3] <= 0 and a[4] == 0 and a[5]==0:
+        break
+
+print('-'*30)
+print(f'돌린 횟수: {count}')
+print(f'걸린 시간: {round(time.time()-start,3)}초')
+print('-'*30)
+print(winner)
+print('-'*30)
+b = [0] * 5
+for i in range(5):
+    if a[i] != 0:
+        b[i] += a[i]
+        print(f'{i}번 나온 숫자가 총 {a[i]}개')
+print('-'*30)
+print(f'#1 {a1}',f'#2 {a2}',f'#3 {a3}',f'#4 {a4}',f'#5 {a5}',sep='\n')
